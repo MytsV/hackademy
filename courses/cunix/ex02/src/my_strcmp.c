e6d69e9726adb4714d91ef0fe05a14550efea743
MytsV <mytsvictoria@gmail.com>
2021-10-28 21:28:03 +0300

    Finished second exercise

---
+++ courses/cunix/ex02/src/my_strcmp.c
@@ -0,0 +1,16 @@
+//the function compares two strings
+int my_strcmp(char *str1, char *str2)
+{
+    for (; *str1 != '\0' && *str2 != '\0'; str1++, str2++) 
+    {
+        if (*str1 < *str2)
+        {
+            return -1;
+        }
+        else if (*str1 != *str2)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
